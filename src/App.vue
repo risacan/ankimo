52f19ad9ed7e862165e5d06f1f8c69b2740838a6
Risa Harada <supikateriburu@gmail.com>
2018-09-24 22:57:51 +0900

    :art: Import bulma

--- src/App.vue
+++ src/App.vue
@@ -14,6 +14,9 @@ export default {
 </script>
 
 <style>
+
+@import url("./assets/bulma.css");
+
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
