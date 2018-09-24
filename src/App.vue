a6b22439e54596e10b00e27072c40ad82887dbbf
Risa Harada <supikateriburu@gmail.com>
2018-09-24 16:50:44 +0900

    :sparkles: Add new page

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <template>
   <div id="app">
     <img src="./assets/logo.png">
+    <p> --- ↓ routingされてるページ --- </p>
     <router-view/>
+    <p> --- ↑ routingされてるページ --- </p>
   </div>
 </template>
 
---
+++ src/components/Buki.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class='buki'>
+   🌟{{title}}🌟
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App',
+      title: 'hoge'
+
+    }
+  }
+}
+</script>
+
+<!-- Add 'scoped' attribute to limit CSS to this component only -->
+<style scoped>
+</style>
