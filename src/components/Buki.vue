789622826b8201413218608e663fac0c0d1234d6
Risa Harada <supikateriburu@gmail.com>
2018-09-24 17:38:32 +0900

    :art: Show buki, sub, special

--- src/components/Buki.vue
+++ src/components/Buki.vue
@@ -1,17 +1,33 @@
 <template>
   <div class='buki'>
-   🌟{{title}}🌟
+    🌟{{title}}🌟
+    <ul id='example-1'>
+      <li v-for='item in data.main' :key="item.key">
+        {{ item.key }}: {{item.localization.ja}}
+        <ul>
+          <li>{{data.sub[item.sub_key].localization.ja}}</li>
+          <li>{{data.special[item.special_key].localization.ja}}</li>
+        </ul>
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
+import Main from '../../static/main.json'
+import Sub from '../../static/sub.json'
+import Special from '../../static/special.json'
 export default {
   name: 'HelloWorld',
   data () {
     return {
       msg: 'Welcome to Your Vue.js App',
-      title: 'hoge'
-
+      title: 'hoge',
+      data: {
+        main: Main,
+        sub: Sub,
+        special: Special
+      }
     }
   }
 }
