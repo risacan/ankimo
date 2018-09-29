05909a0cd57f4c7c82c6a5a6c34ea33e77196a10
Risa Harada <supikateriburu@gmail.com>
2018-09-29 12:32:16 +0900

    :shower: Fix lint

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,36 +1,36 @@
 <template>
-  <div class="hello">
+  <div class='hello'>
     <h1>{{ msg }}</h1>
     <h2>Essential Links</h2>
     <ul>
       <li>
         <a
-          href="https://vuejs.org"
-          target="_blank"
+          href='https://vuejs.org'
+          target='_blank'
         >
           Core Docs
         </a>
       </li>
       <li>
         <a
-          href="https://forum.vuejs.org"
-          target="_blank"
+          href='https://forum.vuejs.org'
+          target='_blank'
         >
           Forum
         </a>
       </li>
       <li>
         <a
-          href="https://chat.vuejs.org"
-          target="_blank"
+          href='https://chat.vuejs.org'
+          target='_blank'
         >
           Community Chat
         </a>
       </li>
       <li>
         <a
-          href="https://twitter.com/vuejs"
-          target="_blank"
+          href='https://twitter.com/vuejs'
+          target='_blank'
         >
           Twitter
         </a>
@@ -38,8 +38,8 @@
       <br>
       <li>
         <a
-          href="http://vuejs-templates.github.io/webpack/"
-          target="_blank"
+          href='http://vuejs-templates.github.io/webpack/'
+          target='_blank'
         >
           Docs for This Template
         </a>
@@ -49,32 +49,32 @@
     <ul>
       <li>
         <a
-          href="http://router.vuejs.org/"
-          target="_blank"
+          href='http://router.vuejs.org/'
+          target='_blank'
         >
           vue-router
         </a>
       </li>
       <li>
         <a
-          href="http://vuex.vuejs.org/"
-          target="_blank"
+          href='http://vuex.vuejs.org/'
+          target='_blank'
         >
           vuex
         </a>
       </li>
       <li>
         <a
-          href="http://vue-loader.vuejs.org/"
-          target="_blank"
+          href='http://vue-loader.vuejs.org/'
+          target='_blank'
         >
           vue-loader
         </a>
       </li>
       <li>
         <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
+          href='https://github.com/vuejs/awesome-vue'
+          target='_blank'
         >
           awesome-vue
         </a>
@@ -85,16 +85,16 @@
 
 <script>
 export default {
-  name: "HelloWorld",
-  data() {
+  name: 'HelloWorld',
+  data () {
     return {
-      msg: "Welcome to Your Vue.js App"
-    };
+      msg: 'Welcome to Your Vue.js App'
+    }
   }
-};
+}
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
+<!-- Add 'scoped' attribute to limit CSS to this component only -->
 <style scoped>
 h1,
 h2 {
