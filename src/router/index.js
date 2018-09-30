651bda1f86b6d0222b0b11e2155ddcb3ff636cff
Risa Harada <supikateriburu@gmail.com>
2018-09-30 13:18:45 +0900

    :wheel_of_dharma: Fix routing

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
 import Buki from '@/components/Buki'
 
 Vue.use(Router)
@@ -10,11 +9,6 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    },
-    {
-      path: '/buki',
       name: 'Buki',
       component: Buki
     }
